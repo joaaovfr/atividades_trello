738dc1931df46aea66cb7067b2555f893dc27988
12345678Ab <joaaovfr@gmail.com>
2024-02-15 22:41:47 -0300

    atualizando exercicios

--- trello5/ex5.js
+++ trello5/ex5.js
@@ -1,5 +1,12 @@
 //Utilize uma estrutura condicional (if,else) para verificar se um número é positivo, 
 //negativo ou zero. Exiba os valores no console.log().
 
-let 
+let numero = prompt('Digite um número')
 
+if (numero > 0) {
+    console.log(` O número é positivo`)
+} else if (numero < 0) {
+    console.log(`O número é negativo`)
+} else {
+    console.log(`ZERO`)
+}
